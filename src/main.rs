3a6cbb6c01a0533928f258d21acd0aaf8acb2338
Kenji Kamada <kamadak@kawai.co.jp>
2022-02-21 12:42:50 +0900

    数値入力ができた

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+   // let mut Za = 3355;
+    let Zb = 2379;
+    let mut amari: [i32; 1000] = [0; 1000];
+    let mut kakeru: [i32; 1000] = [0; 1000];
+    let mut kotae =0; 
+    
+    println!("最大公約数を求める整数を入力してください");
+
+    // 数値の入力
+    let mut number = String::new();
+    io::stdin().read_line(&mut number).ok();
+    let Za = number.trim().parse().ok().unwrap();
+    match Za {
+        _ => println!("{}を入力しました。",Za),
+    }
+                                        // end of 数値の入力
+    
+    kakeru[0] = Za;
+    amari[0] = Zb;
+    for n in 0..=1000 {
+        amari[n+1] = kakeru[n] % amari[n];
+        println!("余り={}",amari[n+1]);
+
+        // 余りが無ければ止める。
+        if amari[n+1]==0{
+            kotae = amari[n];
+            break;
+        }
+        else{
+            kakeru[n+1] = amari[n];
+        }
+
+    }
+
+    println!("最大公約数は...答え = {}",kotae);
 }
