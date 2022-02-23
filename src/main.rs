d7c0255609a0c413f45d7008f2b6c486c35a2d38
Kenji Kamada <kamadak@kawai.co.jp>
2022-02-24 08:22:13 +0900

    if Za>Zb

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,39 @@ use std::io;
 use rand::Rng;
 
 fn main() {
-   // let mut Za = 3355;
+   // let mut z_a = 3355;
   
-    let Zb = rand::thread_rng().gen_range(1, 1000);
-//    let Zb = 2379;
+    let z_b = rand::thread_rng().gen_range(1, 1000);
+//    let z_b = 2379;
     let mut amari: [i32; 1000] = [0; 1000];
     let mut kakeru: [i32; 1000] = [0; 1000];
     let mut kotae =0; 
     
-    println!("{}との最大公約数を求めます。",Zb);
+    println!("{}との最大公約数を求めます。",z_b);
 
     println!("最大公約数を求める整数を入力してください");
 
     // 数値の入力 https://magidropack.hatenablog.com/entry/2018/12/19/131919
     let mut number = String::new();
     io::stdin().read_line(&mut number).ok();
-    let Za = number.trim().parse().ok().unwrap();
-    match Za {
-        _ => println!("{}を入力しました。",Za),
+    let z_a = number.trim().parse().ok().unwrap();
+    match z_a {
+        _ => println!("{}を入力しました。",z_a),
     }
                                         // end of 数値の入力
-    println!("{}と{}の最大公約数を求めます。",Za,Zb);
+    println!("{}と{}の最大公約数を求めます。",z_a,z_b);
     
-    kakeru[0] = Za;
-    amari[0] = Zb;
+    // 大きい方をkakeruに入れる。
+    if z_a > z_b{
+        kakeru[0] = z_a;
+        amari[0] = z_b;
+    }
+    else{
+        kakeru[0] = z_b;
+        amari[0] = z_a;
+    }
+
+    
     for n in 0..=1000 {
         amari[n+1] = kakeru[n] % amari[n];
         println!("余り={}",amari[n+1]);
